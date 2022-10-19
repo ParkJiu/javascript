310815dc8a046289c9b984e87341768e7338578c
Jiu <87464431+Bakjiu@users.noreply.github.com>
2022-10-19 14:52:22 +0900

    imporved coordinates

--- coordinates/main.js
+++ coordinates/main.js
@@ -2,18 +2,18 @@ const vertical = document.querySelector(".vertical");
 const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
+const targetRect = target.getBoundingClientRect();
+console.log(targetRect);
+const targeHalfWidth = targetRect.width / 2;
+const targeHalfHeight = targetRect.height / 2;
 
 document.addEventListener("mousemove", (e) => {
   const x = e.clientX;
   const y = e.clientY;
-
-  console.log(`${x}, ${y}`);
-
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
+  
+  vertical.style.transform = `translateX(${x}px)`;
+  horizontal.style.transform = `translateY(${y}px)`;
+  target.style.transform = `translate(${x-targeHalfWidth}px, ${y-targeHalfHeight}px)`;
+  tag.style.transform = `translate(${x}px, ${y}px)`;
   tag.innerHTML = `${x}px, ${y}px`;
 });
