26af5e4b51f4c4e6c9228a192b536e8385336f0c
Jiu <jiupark25@gmail.com>
2022-10-25 17:29:58 +0900

    HTML Markup, CSS Styling

---
+++ shopping/main.js
@@ -0,0 +1,7 @@
+const inputAdd = document.querySelector('.input__add');
+const addBtn = document.querySelector('button');
+const shoppingList = document.querySelector('.shopping__list');
+
+addBtn.addEventListener('click', () => {
+  console.log('ss');
+})
