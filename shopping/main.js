9e98a32a556862a66526936af649c635223a370a
Jiu <jiupark25@gmail.com>
2022-10-26 14:40:58 +0900

    Add JS

--- shopping/main.js
+++ shopping/main.js
@@ -1,7 +1,65 @@
-const inputAdd = document.querySelector('.input__add');
-const addBtn = document.querySelector('button');
-const shoppingList = document.querySelector('.shopping__list');
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+const removeBtn = document.querySelector('.item__delete');
+const shoppingList = document.querySelector('.item__name');
+
+function onAdd() {
+  // 1. 사용자가 입력한 텍스트를 받아옴
+  const text = input.value;
+  if (text === '') {
+    input.focus();
+    return;
+  }
+  // 2. 새로운 아이템은 만듦 (텍스트 + 삭제 버튼)
+  const item = createItem(text);
+  // 3. items 컨테이너 안에 새로 만든 아이템을 추가
+  items.appendChild(item);
+  // 4. 새로 추가 된 아이템으로 스크롤링
+  item.scrollIntoView({ block: 'center' });
+  // 5. input을 초기화 한다
+  input.value = '';
+  input.focus();
+}
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__Row');
+
+  const item = document.createElement("div");
+  item.setAttribute("class", "item");
+
+  const span = document.createElement("span");
+  span.setAttribute("class", "item__name");
+  span.innerText = text;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item__delete');
+  deleteBtn.innerHTML = `<i class="fas fa-trash"></i>`;
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  })
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
+ 
+  item.appendChild(span);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+
+  return itemRow;
+}
 
 addBtn.addEventListener('click', () => {
-  console.log('ss');
+  onAdd();
+});
+
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    onAdd();
+  }
 })
+
+
