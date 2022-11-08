9e390b3a9b9e134aaaa10fe18db45843a0f74788
Jiu <jiupark25@gmail.com>
2022-11-08 13:38:11 +0900

    modify to form

--- shopping/main.js
+++ shopping/main.js
@@ -3,6 +3,7 @@ const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
 const removeBtn = document.querySelector('.item__delete');
 const shoppingList = document.querySelector('.item__name');
+const form = document.querySelector('.new-form');
 
 function onAdd() {
   // 1. 사용자가 입력한 텍스트를 받아옴
@@ -21,45 +22,41 @@ function onAdd() {
   input.value = '';
   input.focus();
 }
-
+let id = 0; // UUID or HashCode in object 등 고유한 id를 쓰는 것이 좋음
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__Row');
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const span = document.createElement("span");
-  span.setAttribute("class", "item__name");
-  span.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = `<i class="fas fa-trash"></i>`;
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  })
-
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
- 
-  item.appendChild(span);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
-
-  return itemRow;
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = ` 
+        <div class="item">
+          <span class="item__name">${text}</span>
+          <button class="item__delete" >
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+          </button>
+        </div>
+        <div class="item__divider"></div>`;
+    id++;
+    return itemRow;
 }
 
-addBtn.addEventListener('click', () => {
+form.addEventListener('submit', event => {
+  event.preventDefault(); // page자동으로 로딩X
   onAdd();
-});
+})
+// addBtn.addEventListener('click', () => {
+//   onAdd();
+// });
 
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    onAdd();
+// input.addEventListener('keypress', (event) => {
+//   if (event.key === 'Enter') {
+//     onAdd();
+//   }
+// })
+
+items.addEventListener('click', event => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDelete = document.querySelector(`.item__Row[data-id = "${id}"]`);
+    toBeDelete.remove();
   }
 })
-
-
